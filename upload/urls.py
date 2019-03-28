0844f9c46a21eaf1578a7c75d96882a4916ba229
ytian056 <yuan_tian_117@hotmail.com>
2019-03-29 00:32:23 +0800

    phase3

--- upload/urls.py
+++ upload/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^index/$', views.index),
     url(r'^$', views.upload),
+    url(r'^test/$', views.test),
     url(r'^result/$', views.result),
     url(r'^article/(?P<article_id>[0-9]+)/$', views.article_page, name='article_page'),
 ]
--- upload/views.py
+++ upload/views.py
@@ -14,17 +14,213 @@ from . import models
 
 def result(request):
     Dict = [
-                {
-                    'city': 'Beijing',
-                    'country': 'CHN'
-                },
-                {
-                    'city': 'Riverside',
-                    'country': 'USA'
-                }
-            ]
+        {
+            'city': 'Beijing',
+            'country': 'CHN',
+            'next': 'Shijiazhuang',
+            'wayToNext': 'Railway',
+            'flag': 1
+        },
+        {
+            'city': 'Shijiazhuang',
+            'country': 'CHN',
+            'next': 'Taiyuan',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Shijiazhuang',
+            'country': 'CHN',
+            'next': 'Zhengzhou',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Zhengzhou',
+            'country': 'CHN',
+            'next': 'None',
+            'flag': 0,
+        },
+        {
+            'city': 'Zhengzhou',
+            'country': 'CHN',
+            'next': 'Wuhan',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Wuhan',
+            'country': 'CHN',
+            'next': 'Changsha',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Wuhan',
+            'country': 'CHN',
+            'next': 'Chongqing',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Chongqing',
+            'country': 'CHN',
+            'next': 'Chengdu',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Chengdu',
+            'country': 'CHN',
+            'next': 'Guiyang',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Guiyang',
+            'country': 'CHN',
+            'next': 'Guangzhou',
+            'wayToNext': 'Railway',
+            'flag': 3,
+        },
+        {
+            'city': 'Changsha',
+            'country': 'CHN',
+            'next': 'Guangzhou',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Guangzhou',
+            'country': 'CHN',
+            'next': 'None',
+            'flag': 2,
+        },
+        {
+            'city': 'Taiyuan',
+            'country': 'CHN',
+            'next': "Xi'an",
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': "Xi'an",
+            'country': 'CHN',
+            'next': 'Chengdu',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': "Zhengzhou",
+            'country': 'CHN',
+            'next': "Xi'an",
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+    ]
+
+
+    Dict_ = [
+        {
+            'city': 'Beijing',
+            'country': 'CHN',
+            'next': 'Shijiazhuang',
+            'wayToNext': 'Railway',
+            'flag': 1,
+        },
+        {
+            'city': 'Shijiazhuang',
+            'country': 'CHN',
+            'next': 'Taiyuan',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Shijiazhuang',
+            'country': 'CHN',
+            'next': 'Zhengzhou',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Zhengzhou',
+            'country': 'CHN',
+            'next': 'None',
+            'flag': 0,
+        },
+        {
+            'city': 'Zhengzhou',
+            'country': 'CHN',
+            'next': 'Wuhan',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Wuhan',
+            'country': 'CHN',
+            'next': 'Changsha',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Wuhan',
+            'country': 'CHN',
+            'next': 'Chongqing',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Chongqing',
+            'country': 'CHN',
+            'next': 'Chengdu',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Chengdu',
+            'country': 'CHN',
+            'next': 'Guangzhou',
+            'wayToNext': 'Airplane',
+            'flag': 0,
+        },
+        {
+            'city': 'Changsha',
+            'country': 'CHN',
+            'next': 'Guangzhou',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': 'Guangzhou',
+            'country': 'CHN',
+            'next': 'None',
+            'flag': 2,
+        },
+        {
+            'city': 'Taiyuan',
+            'country': 'CHN',
+            'next': "Xi'an",
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': "Xi'an",
+            'country': 'CHN',
+            'next': 'Chengdu',
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+        {
+            'city': "Zhengzhou",
+            'country': 'CHN',
+            'next': "Xi'an",
+            'wayToNext': 'Railway',
+            'flag': 0,
+        },
+    ]
     return render(request, 'upload/result.html', {
-        'Dict': json.dumps(Dict)
+        'Dict': json.dumps(Dict),
+        'Dict_': json.dumps(Dict_)
     })
 
 def index(request):
@@ -35,9 +231,190 @@ def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, 'upload/article_page.html', {'article':article})
 
+def test(request):
+    Dict = [
+        {
+            'city': 'Beijing',
+            'country': 'CHN',
+            'next': 'Shijiazhuang',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Shijiazhuang',
+            'country': 'CHN',
+            'next': 'Taiyuan',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Shijiazhuang',
+            'country': 'CHN',
+            'next': 'Zhengzhou',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Zhengzhou',
+            'country': 'CHN',
+            'next': 'None',
+        },
+        {
+            'city': 'Zhengzhou',
+            'country': 'CHN',
+            'next': 'Wuhan',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Wuhan',
+            'country': 'CHN',
+            'next': 'Changsha',
+        },
+        {
+            'city': 'Wuhan',
+            'country': 'CHN',
+            'next': 'Chongqing',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Chongqing',
+            'country': 'CHN',
+            'next': 'Chengdu',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Chengdu',
+            'country': 'CHN',
+            'next': 'Guiyang',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Guiyang',
+            'country': 'CHN',
+            'next': 'Guangzhou',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Changsha',
+            'country': 'CHN',
+            'next': 'Guangzhou',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Guangzhou',
+            'country': 'CHN',
+            'next': 'None',
+        },
+        {
+            'city': 'Taiyuan',
+            'country': 'CHN',
+            'next': "Xi'an",
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': "Xi'an",
+            'country': 'CHN',
+            'next': 'Chengdu',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': "Zhengzhou",
+            'country': 'CHN',
+            'next': "Xi'an",
+            'wayToNext': 'Railway'
+        },
+    ]
+
+    Dict_ = [
+        {
+            'city': 'Beijing',
+            'country': 'CHN',
+            'next': 'Shijiazhuang',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Shijiazhuang',
+            'country': 'CHN',
+            'next': 'Taiyuan',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Shijiazhuang',
+            'country': 'CHN',
+            'next': 'Zhengzhou',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Zhengzhou',
+            'country': 'CHN',
+            'next': 'None',
+        },
+        {
+            'city': 'Zhengzhou',
+            'country': 'CHN',
+            'next': 'Wuhan',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Wuhan',
+            'country': 'CHN',
+            'next': 'Changsha',
+        },
+        {
+            'city': 'Wuhan',
+            'country': 'CHN',
+            'next': 'Chongqing',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Chongqing',
+            'country': 'CHN',
+            'next': 'Chengdu',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Chengdu',
+            'country': 'CHN',
+            'next': 'Guangzhou',
+            'wayToNext': 'Airplane'
+        },
+        {
+            'city': 'Changsha',
+            'country': 'CHN',
+            'next': 'Guangzhou',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': 'Guangzhou',
+            'country': 'CHN',
+            'next': 'None',
+        },
+        {
+            'city': 'Taiyuan',
+            'country': 'CHN',
+            'next': "Xi'an",
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': "Xi'an",
+            'country': 'CHN',
+            'next': 'Chengdu',
+            'wayToNext': 'Railway'
+        },
+        {
+            'city': "Zhengzhou",
+            'country': 'CHN',
+            'next': "Xi'an",
+            'wayToNext': 'Railway'
+        },
+    ]
+
+
+    return render(request, 'upload/test.html', {'Dict': json.dumps(Dict), 'Dict_':json.dumps(Dict_)})
+
 def upload(request):
     if request.method == 'POST':# 获取对象
-        obj = request.FILES.get('document')
-        print(obj.name)
-        print(obj.size)
+        obj1 = request.FILES.get('document1')
+        print(obj1.name)
+        print(obj1.size)
+        obj2 = request.FILES.get('document2')
+        print(obj2.name)
+        print(obj2.size)
     return render(request, 'upload/upload.html')
